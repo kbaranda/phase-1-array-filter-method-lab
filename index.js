9216917300aefac96696301dad9910568f7bd9de
Kat <bolivarkat@yahoo.com>
2021-09-09 21:56:49 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, str) {
+    return drivers.filter(array => array.toUpperCase() === str.toUpperCase())
+}
+
+function fuzzyMatch(drivers, str) {
+    return drivers.filter(array => array.substring(0,1).toUpperCase() === str.substring(0,1).toUpperCase())
+}
+
+function matchName(drivers, str) {
+    return drivers.filter(array => array.name === str)
+}
